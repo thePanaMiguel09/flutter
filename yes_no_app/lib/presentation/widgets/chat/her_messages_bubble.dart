1f7773f2143a561555d7336a2d296d56b275aab2
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-09-29 00:39:32 -0500

    Image loader

--- yes_no_app/lib/presentation/widgets/chat/her_messages_bubble.dart
+++ yes_no_app/lib/presentation/widgets/chat/her_messages_bubble.dart
@@ -29,8 +29,6 @@ class HerMessageBubble extends StatelessWidget {
 }
 
 class _ImageBubble extends StatelessWidget {
-  const _ImageBubble({super.key});
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -41,6 +39,18 @@ class _ImageBubble extends StatelessWidget {
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) {
+            return child;
+          }
+
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: const Text('enviando mensaje...'),
+          );
+        },
       ),
     );
   }
