4411397ec8fbb25fd09a7d57b9a167c1afc0da19
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-09-30 23:45:15 -0500

    yes_no model mapped

--- yes_no_app/lib/config/helpers/get_yes_no_answer.dart
+++ yes_no_app/lib/config/helpers/get_yes_no_answer.dart
@@ -1,12 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/infraestructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer {
   final _dio = Dio();
 
   Future<Message> getAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
-    
-    throw UnimplementedError();
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+
+    return yesNoModel.toMessageEntity();
   }
 }
