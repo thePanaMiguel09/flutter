c0860bb18010be184d194a819483e731e55d2bd2
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-09-30 23:05:18 -0500

    Chat scroll animation

--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messages = [
     Message(text: 'Hola hermanilla', fromWho: FromWho.me),
     Message(text: 'Hola hermanito', fromWho: FromWho.her),
@@ -9,8 +11,20 @@ class ChatProvider extends ChangeNotifier {
   ];
 
   Future<void> sendMessage(String message) async {
+    if (message.isEmpty) return;
     final newMessage = Message(text: message, fromWho: FromWho.me);
     messages.add(newMessage);
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 }
