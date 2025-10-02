7936de5fdc399f1ecf0f56d4babd6337b4dd2608
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-01 23:00:25 -0500

    Video player and android manifest

--- tik_tok_app/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ tik_tok_app/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tik_tok_app/domain/entities/video_post.dart';
 import 'package:tik_tok_app/presentation/widgets/shared/video_buttons.dart';
+import 'package:tik_tok_app/presentation/widgets/video/full_screen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -16,6 +17,12 @@ class VideoScrollableView extends StatelessWidget {
         final VideoPost videoPost = videos[index];
         return Stack(
           children: [
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                caption: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+              ),
+            ),
             Positioned(
               bottom: 40,
               right: 20,
