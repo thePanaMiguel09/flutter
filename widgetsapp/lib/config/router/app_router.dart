24740d1d599b3952b7dbfa2689677c420817ba24
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-05 18:59:53 -0500

    go router

---
+++ widgetsapp/lib/config/router/app_router.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgetsapp/presentation/screens.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/',
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/home',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomeScreen();
+      },
+    ),
+    GoRoute(
+      path: '/buttons',
+      builder: (BuildContext context, GoRouterState state) {
+        return const ButtonsScreen();
+      },
+    ),
+    GoRoute(
+      path: '/cards',
+      builder: (BuildContext context, GoRouterState state) {
+        return const CardScreen();
+      },
+    ),
+  ],
+);
---
+++ widgetsapp/lib/presentation/screens.dart
@@ -0,0 +1,3 @@
+export 'package:widgetsapp/presentation/screens/button/buttons_screen.dart';
+export 'package:widgetsapp/presentation/screens/card/card_screen.dart';
+export 'package:widgetsapp/presentation/screens/home/home_screen.dart';
