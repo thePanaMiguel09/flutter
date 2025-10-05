2e067aa16f73090c0eaf379944d9cbe098292dde
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-05 01:05:54 -0500

    HomeScreen implmented with styles

--- widgetsapp/lib/presentation/screens/home/home_screen.dart
+++ widgetsapp/lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgetsapp/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,45 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Widgets')));
+      appBar: AppBar(title: const Text('Flutter Widgets')),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+
+        return _CustomeListTitle(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomeListTitle extends StatelessWidget {
+  const _CustomeListTitle({required this.menuItem});
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_outlined),
+      onTap: () {
+        //TODO navigate to another page
+      },
+    );
   }
 }
