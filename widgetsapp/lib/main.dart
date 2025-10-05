256761139321ea138373ff5ca21cd5ba495e49ee
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-05 00:40:57 -0500

    AppTheme

--- widgetsapp/lib/main.dart
+++ widgetsapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgetsapp/config/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 3).getTheme(),
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Material App Bar')),
