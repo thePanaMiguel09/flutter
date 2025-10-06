32e7aceda70dbed5d78553dfd6ac04e96f26cb3c
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-05 20:14:46 -0500

    cards and singlechild scroll view

--- widgetsapp/lib/presentation/screens/card/card_screen.dart
+++ widgetsapp/lib/presentation/screens/card/card_screen.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+const cards = <Map<String, dynamic>>[
+  {'elevetion': 0.0, 'label': 'Elevation 1'},
+
+  {'elevetion': 1.0, 'label': 'Elevation 2'},
+
+  {'elevetion': 2.0, 'label': 'Elevation 3'},
+
+  {'elevetion': 3.0, 'label': 'Elevation 4'},
+
+  {'elevetion': 4.0, 'label': 'Elevation %'},
+];
 
 class CardScreen extends StatelessWidget {
   static const String name = 'card_screen';
@@ -9,7 +22,66 @@ class CardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Cards')),
-      body: Placeholder(),
+      body: CardsView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.pop();
+        },
+        child: Icon(Icons.arrow_back_ios_sharp),
+      ),
+    );
+  }
+}
+
+class CardsView extends StatelessWidget {
+  const CardsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...cards.map(
+            (c) => CardType1(label: c['label'], cardElevation: c['elevetion']),
+          ),
+          ...cards.map(
+            (c) => CardType1(label: c['label'], cardElevation: c['elevetion']),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CardType1 extends StatelessWidget {
+  final String label;
+  final double cardElevation;
+
+  const CardType1({
+    super.key,
+    required this.label,
+    required this.cardElevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: cardElevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
     );
   }
 }
