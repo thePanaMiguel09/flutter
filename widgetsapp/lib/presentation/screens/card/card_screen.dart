bce5c93ea482881f3afbde8b51f419702d4b33c1
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-10-05 20:35:05 -0500

    custom card with image

--- widgetsapp/lib/presentation/screens/card/card_screen.dart
+++ widgetsapp/lib/presentation/screens/card/card_screen.dart
@@ -45,8 +45,15 @@ class CardsView extends StatelessWidget {
             (c) => CardType1(label: c['label'], cardElevation: c['elevetion']),
           ),
           ...cards.map(
-            (c) => CardType1(label: c['label'], cardElevation: c['elevetion']),
+            (c) => CardType2(label: c['label'], cardElevation: c['elevetion']),
           ),
+          ...cards.map(
+            (c) => CardType3(label: c['label'], cardElevation: c['elevetion']),
+          ),
+          ...cards.map(
+            (c) => CardType4(label: c['label'], cardElevation: c['elevetion']),
+          ),
+          SizedBox(height: 50),
         ],
       ),
     );
@@ -81,6 +88,125 @@ class CardType1 extends StatelessWidget {
             Align(alignment: Alignment.bottomLeft, child: Text(label)),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CardType2 extends StatelessWidget {
+  final String label;
+  final double cardElevation;
+
+  const CardType2({
+    super.key,
+    required this.label,
+    required this.cardElevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+        side: BorderSide(color: color.outline),
+      ),
+
+      elevation: cardElevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardType3 extends StatelessWidget {
+  final String label;
+  final double cardElevation;
+
+  const CardType3({
+    super.key,
+    required this.label,
+    required this.cardElevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+
+    return Card(
+      color: color.surfaceContainerHighest,
+      elevation: cardElevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardType4 extends StatelessWidget {
+  final String label;
+  final double cardElevation;
+
+  const CardType4({
+    super.key,
+    required this.label,
+    required this.cardElevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: cardElevation,
+      child: Stack(
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${cardElevation.toInt()}/600/300',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                ),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert_outlined),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
